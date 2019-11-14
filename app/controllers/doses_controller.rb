dbdd65f20326f449bf5eb8194344a15d54fbfb03
ronan querrec <ronan_querrec@yahoo.com>
2019-11-14 16:52:01 +0100

    cocktails

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+
+  def new
+
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktails_path
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.find(params[:cocktail_id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
+
